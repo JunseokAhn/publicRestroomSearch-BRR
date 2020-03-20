50adc11acde2699843f0d63e1bba3de88ffa9d17
hjh9090 <zolaboy1882@gmail.com>
2020-03-21 03:00:14 +0900

    네이버 로그인 JSON객체에서 DB로 입력 성공

--- src/main/java/global/sesoc/brr/dao/NaverLoginDAO.java
+++ src/main/java/global/sesoc/brr/dao/NaverLoginDAO.java
@@ -1,10 +1,33 @@
 package global.sesoc.brr.dao;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import global.sesoc.brr.mapper.NaverLoginMapper;
+import global.sesoc.brr.vo.NaverVO;
 
 @Repository
 public class NaverLoginDAO {
 
+	@Autowired
 	private SqlSession session;
+	
+	//네이버에서 가지고 온 값들을 서버에 저장시킴
+	public int insertNaver (NaverVO naver) {
+		NaverLoginMapper mapper = session.getMapper(NaverLoginMapper.class);
+			int result = 0;
+		try {
+			result = mapper.insertNaver(naver);
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.rollback();
+		}
+		return result;
+	}
+	
+	
+	
+	
+	
 }
--- src/main/java/global/sesoc/brr/mapper/NaverLoginMapper.java
+++ src/main/java/global/sesoc/brr/mapper/NaverLoginMapper.java
@@ -1,5 +1,11 @@
 package global.sesoc.brr.mapper;
 
+import global.sesoc.brr.vo.NaverVO;
+
 public interface NaverLoginMapper {
 
+	
+	//네이버 회원 정보 JSON객체를 DB에 저장
+	public int insertNaver (NaverVO naver);
+	
 }
