5ddebf50afe622ddaf5c56917867a72772714c49
JunseokAhn <57289429+JunseokAhn@users.noreply.github.com>
2020-04-01 14:50:14 +0900

    에이잭스로 맵스메인이랑 맵스컨트롤러 연결

--- src/main/java/global/sesoc/brr/controller/GoogleMapsController.java
+++ src/main/java/global/sesoc/brr/controller/GoogleMapsController.java
@@ -6,7 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -16,15 +18,28 @@ public class GoogleMapsController {
 
 	@GetMapping(value = "mapsMain")
 	public String MapsMain(Model model) {
-		RestTemplate restTemplate = new RestTemplate();
-		URI uri = UriComponentsBuilder.fromHttpUrl(//길찾기경로 origin=출발지 destination=도착지인것같다.
-				"https://maps.googleapis.com/maps/api/directions/json?origin=41.43206,-81.38992&destination=41.43206,-81.38992&key=AIzaSyDkQ00U2AUBQSS1CJF5YveL-1YWsTjaRGA")
-				.queryParam("key", "value").build().toUri();
-		// String response = restTemplate.getForObject(uri, String.class); 또는
-		ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
-		System.out.println(responseEntity);
-		model.addAttribute("directions", responseEntity);
+
+		/*
+		 * 데이터파싱연습 RestTemplate restTemplate = new RestTemplate(); URI uri =
+		 * UriComponentsBuilder.fromHttpUrl(//길찾기경로 origin=출발지
+		 * destination=도착지인것같다.
+		 * "https://maps.googleapis.com/maps/api/directions/json?origin=41.43206,-81.38992&destination=41.43206,-81.38992&key=AIzaSyDkQ00U2AUBQSS1CJF5YveL-1YWsTjaRGA")
+		 * .queryParam("key", "value").build().toUri(); // String response =
+		 * restTemplate.getForObject(uri, String.class); 또는
+		 * ResponseEntity<String> responseEntity =
+		 * restTemplate.getForEntity(uri, String.class);
+		 * System.out.println(responseEntity); model.addAttribute("directions",
+		 * responseEntity);
+		 */
 		return "maps/mapsMain";
+	}
+
+	@ResponseBody
+	@PostMapping(value = "getLocation")
+	public String getLocation(String lat, String lng) {
+		System.out.println(lat);
+		System.out.println(lng);
+		return lat;
 	}
 
 }
