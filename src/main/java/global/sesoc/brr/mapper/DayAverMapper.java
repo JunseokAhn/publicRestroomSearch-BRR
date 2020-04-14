15066bbb06522bf1f594fdf2d6d908a7e6d1e080
hjh9090 <zolaboy1882@gmail.com>
2020-04-14 23:17:13 +0900

    화장실이용자현황틀만듬(목요일회의후나머지완성예정)

--- src/main/java/global/sesoc/brr/mapper/DayAverMapper.java
+++ src/main/java/global/sesoc/brr/mapper/DayAverMapper.java
@@ -1,5 +1,7 @@
 package global.sesoc.brr.mapper;
 
+import java.util.ArrayList;
+
 import global.sesoc.brr.vo.DayAverVO;
 
 public interface DayAverMapper {
@@ -7,5 +9,9 @@ public interface DayAverMapper {
 	//길을 찾았을 때 해당 길찾기 내용 서버 저장
 	public int insertAver (DayAverVO aver);
 	
+	//하루 이용한 사람들의 정보들을 서버에서 가지고 옴
+	public ArrayList<DayAverVO> listAll ();
 	
+	//해당 화장실의 평균 이용량
+	public ArrayList<DayAverVO> getAver (String toiletnm);
 }
