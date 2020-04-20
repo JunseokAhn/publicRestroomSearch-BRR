17a42e396d9935001979c182939ce7d38096ab69
JunseokAhn <57289429+JunseokAhn@users.noreply.github.com>
2020-04-20 15:23:49 +0900

    리뷰 테이블쿼리l추가

---
+++ review.sql
@@ -0,0 +1,10 @@
+CREATE TABLE brr_review(
+    toilet_title varchar2(100),
+    sessionId varchar2(100),
+    nickname varchar2(100),
+    picktureUrl varchar2(1000),
+    profile_image varchar2(1000),
+    star number,
+    clean number,
+    inpudate date default sysdate
+);
